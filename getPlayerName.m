137dbca6532eb0dc01ffd4e5ceb870d8f3c2efe1
George Ehrhorn <ehrhorn@gmail.com>
2016-01-03 14:55:53 -0500

    Minor formatting

--- getPlayerName.m
+++ getPlayerName.m
@@ -1,6 +1,6 @@
 function [ N ] = getPlayerName( T )
 %getPlayerName Return data on player names
-%   Detailed explanation goes here
+
     load('baseballstats.mat', 'Master');
     [~, idx] = ismember(T, Master.playerID);
     N = Master(idx, {'nameFirst' 'nameLast'});
