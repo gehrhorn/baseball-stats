ab302165b6b980e1ea501979f9550917c0fc4347
George Ehrhorn <ehrhorn@gmail.com>
2016-01-05 19:47:58 -0500

    Added wRAA

--- calculateWRAA.m
+++ calculateWRAA.m
@@ -2,9 +2,11 @@ function [ wRAA ] = calculateWRAA( B, W )
 %calculateWRAA Summary of this function goes here
 %   Detailed explanation goes here
     W.Properties.VariableNames{'wOBA'} = 'lgWOBA';
+
     T = join(B, W, 'RightVariables', {'lgWOBA', 'wOBAScale'});
     wRAA = ((T.wOBA - T.lgWOBA) ./ T.wOBAScale) .* ...
         double(T.AB + T.BB + T.HBP + T.SH + T.SF);
+
 
 end
 
